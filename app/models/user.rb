3fe4ac970429da027579c7e565ef4bb4b2bbb334
AgustinPalmaM <palma.agustin7@gmail.com>
2023-08-22 16:38:07 -0400

    Config registration devise view to ask also first_name, last_name and date_of_birth to sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,5 +22,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+  validates :first_name, :last_name, :date_of_birth, presence: true
 end
