653bd1b51769ce99470ba2f525fc9fdd6c5e3751
AgustinPalmaM <palma.agustin7@gmail.com>
2023-08-22 16:22:32 -0400

    Add annotate Add columns first_name - last_name - date_of_birth to user model Add has_one_attach to user

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id          :bigint           not null, primary key
+#  business_id :bigint           not null
+#  name        :string
+#  balance     :integer
+#  currency    :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Account < ApplicationRecord
   belongs_to :business
   has_many :movements
--- test/models/movement_test.rb
+++ test/models/movement_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: movements
+#
+#  id          :bigint           not null, primary key
+#  accounts_id :bigint           not null
+#  balance     :integer
+#  currency    :string
+#  category    :integer
+#  beneficiary :string
+#  note        :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class MovementTest < ActiveSupport::TestCase
