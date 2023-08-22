9b19fe8707cd6051f7ed7e99670b776f875267e7
AgustinPalmaM <palma.agustin7@gmail.com>
2023-08-22 16:46:49 -0400

    Add relation has_many :businesses to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@
 class User < ApplicationRecord
 
   has_one_attached :avatar
+  has_many :businesses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
