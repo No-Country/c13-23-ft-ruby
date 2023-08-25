754ec4023b839a038ecc22211bb92fb86ffda423
AgustinPalmaM <palma.agustin7@gmail.com>
2023-08-25 03:46:41 -0400

    Add sign_up and sign_in with google option

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,13 +15,22 @@
 #  date_of_birth          :date
 #
 class User < ApplicationRecord
-
-  has_one_attached :avatar
+  # has_one_attached :avatar
   has_many :businesses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
-  validates :first_name, :last_name, :date_of_birth, presence: true
+  # validates :first_name, :last_name, :date_of_birth, presence: true
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    user ||= User.create( email: data['email'],
+                          password: Devise.friendly_token[0, 20])
+    user
+  end
 end
