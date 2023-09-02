f784b865b47d89c76b64cbf63430cd8f627c867e
AgustinPalmaM <palma.agustin7@gmail.com>
2023-09-02 17:52:01 -0400

    Add edit feature to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name date_of_birth avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name date_of_birth avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name date_of_birth avatar phone])
   end
 end
