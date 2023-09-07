14893034efb01fb094891fafcb8fd97836dac17e
AgustinPalmaM <palma.agustin7@gmail.com>
2023-09-07 11:45:02 -0300

    Add redirect_to user profile after sign_in

--- app/controllers/registrations/sessions_controller.rb
+++ app/controllers/registrations/sessions_controller.rb
@@ -3,15 +3,12 @@
 class Registrations::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   
+  
 
   # GET /resource/sign_in
-  def new
-    super
-    puts "@@@@@@@@@@@@"
-    @route = params[:controller]
-    puts @route
-    puts "@@@@@@@@@@@@"
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
   # def create
