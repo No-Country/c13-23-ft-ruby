9513c06ba69e96fd9fbad939e5f8ab79a77a5948
Marcela Fernández <marferpu17@gmail.com>
2023-09-14 02:34:11 -0500

    controller and view to transfers

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,13 +1,20 @@
 class TransfersController < ApplicationController
-    before_action :set_movement
+    # before_action :set_movement
   
     def index
-      @transfers = @movement.transfer
+      @bussines = Business.find(params[:business_id])
+      @accounts = @bussines.accounts
+      @transfers = []
+      @accounts.map do |account|
+        account.movements.map do |movement|
+          @earnings.push(movement) if movement.beneficiary == 'Transferencia'
+        end
+      end        
     end
   
     private
   
-    def set_movement
-      @movement = Movement.find(params[:movement_id])
-    end
+    # def set_movement
+    #   @movement = Movement.find(params[:movement_id])
+    # end
   end
