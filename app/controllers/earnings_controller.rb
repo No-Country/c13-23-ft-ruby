606863f3fa6227aa5a1dde622560afb8b420fffa
AgustinPalmaM <palma.agustin7@gmail.com>
2023-09-14 23:12:41 -0300

    Resolve fixes into eaning controller

--- app/controllers/earnings_controller.rb
+++ app/controllers/earnings_controller.rb
@@ -1,22 +1,24 @@
 class EarningsController < ApplicationController
 # before_action :set_movement, only: %i[index]
   def index
-    @bussines = Business.find(params[:business_id])
-    @accounts = @bussines.accounts
+    @business = Business.find(params[:business_id])
+    @accounts = @business.accounts
     @earnings = []
     @accounts.map do |account|
       account.movements.map do |movement|
         @earnings.push(movement) if movement.beneficiary == 'Ingreso'
       end
-    end
+    end        
   end
 
   def new
-    @bussines = Business.find(params[:business_id])
-    @accounts = @bussines.accounts
+    # @egress = Egress.new
+    @business = Business.find(params[:business_id])
+    @accounts = @business.accounts
   end
 
-  private
+
+
 
   # def set_movement
   # @movement = Movement.find(params[:movement_id])
