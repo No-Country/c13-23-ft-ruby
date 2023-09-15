b77391a305d98999f06758d6632cf521b3210594
Giovanny Salcedo <gsalcedo26@gmail.com>
2023-09-14 20:48:08 -0500

    fixed typos earningss

--- app/controllers/earnings_controller.rb
+++ app/controllers/earnings_controller.rb
@@ -1,8 +1,8 @@
 class EarningsController < ApplicationController
 # before_action :set_movement, only: %i[index]
   def index
-    @bussines = Business.find(params[:business_id])
-    @accounts = @bussines.accounts
+    @business = Business.find(params[:business_id])
+    @accounts = @business.accounts
     @earnings = []
     @accounts.map do |account|
       account.movements.map do |movement|
@@ -12,13 +12,7 @@ class EarningsController < ApplicationController
   end
 
   def new
-    @bussines = Business.find(params[:business_id])
-    @accounts = @bussines.accounts
+    @business = Business.find(params[:business_id])
+    @accounts = @business.accounts
   end
-
-  private
-
-  # def set_movement
-  # @movement = Movement.find(params[:movement_id])
-  # end
 end
