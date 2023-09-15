82a0da90b3ba02d8cc85e1e612134dd8c6e27285
Giovanny Salcedo <gsalcedo26@gmail.com>
2023-09-14 20:24:57 -0500

    view table transfers done

--- app/controllers/earnings_controller.rb
+++ app/controllers/earnings_controller.rb
@@ -1,25 +1,24 @@
 class EarningsController < ApplicationController
-    # before_action :set_movement, only: %i[index]
-  
-    def index
-      @bussines = Business.find(params[:business_id])
-      @accounts = @bussines.accounts
-      @earnings = []
-      @accounts.map do |account|
-        account.movements.map do |movement|
-          @earnings.push(movement) if movement.beneficiary == 'Ingreso'
-        end
-      end        
+# before_action :set_movement, only: %i[index]
+  def index
+    @bussines = Business.find(params[:business_id])
+    @accounts = @bussines.accounts
+    @earnings = []
+    @accounts.map do |account|
+      account.movements.map do |movement|
+        @earnings.push(movement) if movement.beneficiary == 'Ingreso'
+      end
     end
-    
-    def new
-      @bussines = Business.find(params[:business_id])
-      @accounts = @bussines.accounts
-    end
-      private
-    
-      # def set_movement
-      #   @movement = Movement.find(params[:movement_id])
-      # end
   end
-  
+
+  def new
+    @bussines = Business.find(params[:business_id])
+    @accounts = @bussines.accounts
+  end
+
+  private
+
+  # def set_movement
+  # @movement = Movement.find(params[:movement_id])
+  # end
+end
