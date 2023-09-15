0882f50f39108d782865dc8d56983c418cdcb03f
Giovanny Salcedo <gsalcedo26@gmail.com>
2023-09-15 01:26:33 -0500

    c74783d325090a7f07749959cd157dae24b7a910

--- app/controllers/egresses_controller.rb
+++ app/controllers/egresses_controller.rb
@@ -1,29 +1,27 @@
 class EgressesController < ApplicationController
-    # before_action :set_movement, only: %i[index]
-  
-    def index
-      @bussines = Business.find(params[:business_id])
-      @accounts = @bussines.accounts
-      @egresses = []
-      @accounts.map do |account|
-        account.movements.map do |movement|
-          @egresses.push(movement) if movement.beneficiary == 'Egreso'
-        end
-      end        
-    end
+  # before_action :set_movement, only: %i[index]
 
-    def new
-      # @egress = Egress.new
-      @business = Business.find(params[:business_id])
-      @accounts = @business.accounts
-      @categorias = Movement.categorias_gastos.keys.map { |categoria| [categoria, categoria]}
+  def index
+    @bussines = Business.find(params[:business_id])
+    @accounts = @bussines.accounts
+    @egresses = []
+    @accounts.map do |account|
+      account.movements.map do |movement|
+        @egresses.push(movement) if movement.beneficiary == 'Egreso'
+      end
     end
-    
-  
-    # private
-
-    # def set_movement
-    #     @movement = Movement.find(params[:movement_id])
-    # end
   end
-  
+
+  def new
+    # @egress = Egress.new
+    @business = Business.find(params[:business_id])
+    @accounts = @business.accounts
+    @categorias = Movement.categorias_gastos.keys.map { |categoria| [categoria, categoria]}
+  end
+
+# private
+
+# def set_movement
+#   @movement = Movement.find(params[:movement_id])
+# end
+end
