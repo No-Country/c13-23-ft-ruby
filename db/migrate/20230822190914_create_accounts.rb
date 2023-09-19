c2c0f2536641516993b88297a63e9974c4188aff
Giovanny Salcedo <gsalcedo26@gmail.com>
2023-09-18 22:53:49 -0500

    configured models for balance

--- db/migrate/20230822190914_create_accounts.rb
+++ db/migrate/20230822190914_create_accounts.rb
@@ -3,7 +3,7 @@ class CreateAccounts < ActiveRecord::Migration[7.0]
     create_table :accounts do |t|
       t.references :business, null: false, foreign_key: true
       t.string :name
-      t.integer :balance_cents, default: 0
+      t.integer :balance, default: 0
 
       t.timestamps
     end
